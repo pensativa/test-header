0a5865e31150589f762316869b1b3b1bd010cf6b
Mariia Opanasenko <mkofein@gmail.com>
2025-02-21 22:19:50 +0200

    Small fixes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,15 +1,13 @@
 // Open/close mobile menu and submenu
 const header = document.querySelector('.header');
 const submenuBtns = document.querySelectorAll('.nav__item-has-child .nav__link');
-const  toggleMenu = document.querySelectorAll('.toggle-menu');
+const  toggleMenu = document.querySelector('.toggle-menu');
 const body = document.querySelector('body');
 
-toggleMenu.forEach(btn => {
-    btn.addEventListener('click', () => {
-        header.classList.toggle('open');
-        btn.classList.toggle('open');
-        body.classList.toggle('overflow-hidden');
-    });
+toggleMenu.addEventListener('click', () => {
+    header.classList.toggle('open');
+    toggleMenu.classList.toggle('open');
+    body.classList.toggle('overflow-hidden');
 });
 
 if (window.innerWidth < 1200) {
@@ -27,10 +25,12 @@ if (window.innerWidth < 1200) {
 const langBtn = document.querySelector('.languages__current');
 const langMenu = document.querySelector('.menu__list');
 
-langBtn.addEventListener('click', () => {
+langBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     langMenu.classList.toggle('open');
     langBtn.classList.toggle('open');
 });
+
 
 // Desctop search form
 const searchBtn = document.getElementById('searchOpen');
@@ -44,9 +44,23 @@ searchBtn.addEventListener('click', (e) => {
 });
 
 document.addEventListener('click', function(event) {
-    const isClickInside = formSearch.contains(event.target);
-    if (!isClickInside) {
+    const isClickInsideSearch = formSearch.contains(event.target);
+    if (!isClickInsideSearch) {
         formSearch.classList.remove('open');
         searchText.classList.remove('hidden');
+    }
+
+    const isClickInsidelang = langBtn.contains(event.target);
+    if (!isClickInsidelang) {
+        langMenu.classList.remove('open');
+        langBtn.classList.remove('open');
+    }
+
+    const isClickInsideMenu = document.querySelector('.header__nav').contains(event.target);
+    const isClickInsideMenuToogle = toggleMenu.contains(event.target);
+    if (!isClickInsideMenu && !isClickInsideMenuToogle) {
+        header.classList.remove('open');
+        toggleMenu.classList.remove('open');
+        body.classList.remove('overflow-hidden');
     }
 });
